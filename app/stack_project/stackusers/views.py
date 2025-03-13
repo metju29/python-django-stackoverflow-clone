279bf71e3aedfbbda97781c3446891869a746dd2
metju29 <metju29@gmail.com>
2025-03-13 17:25:18 +0100

    Finished profile update system

--- app/stack_project/stackusers/views.py
+++ app/stack_project/stackusers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import user_passes_test, login_required
+from .forms import ProfileUpdateForm, UserUpdateForm
 
 
 def not_logged_in(user):
@@ -24,3 +25,22 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'stackusers/profile.html')
+
+def profile_update(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account updated successfully.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'stackusers/profile_update.html', context)
